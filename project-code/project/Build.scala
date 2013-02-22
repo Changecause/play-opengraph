6460fb2ea39f4fe1aab437f1c4fb57a9d15165ab
Thomas Felix <code@tfelix.de>
2013-02-22 01:56:41 +0100

    Migrated to Play 2.1.

--- project-code/project/Build.scala
+++ project-code/project/Build.scala
@@ -5,13 +5,13 @@ import PlayProject._
 object ApplicationBuild extends Build {
 
     val appName         = "opengraph-module"
-    val appVersion      = "0.1.2-SNAPSHOT"
+    val appVersion      = "0.1.3-SNAPSHOT"
 
     val appDependencies = Seq(
-      // Add your project dependencies here,
+    	javaCore
     )
 
-    val main = PlayProject(appName, appVersion, appDependencies, mainLang = JAVA).settings(
+    val main = play.Project(appName, appVersion, appDependencies).settings(
     	organization := "de.tfelix",
 		javacOptions ++= Seq("-target", "1.6") ++ Seq("-source", "1.6"),
 		publishTo := Some(Resolver.file("file",  new File( "C:/test" )) )
